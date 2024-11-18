794695d7b8b3b13eee0820831b85b2cab8806fdc
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-18 18:19:52 +0200

    coupon controller

---
+++ SmartMenu.Web/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SmartMenu.Web.Models;
+using SmartMenu.Web.Service.IService;
+
+namespace SmartMenu.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
