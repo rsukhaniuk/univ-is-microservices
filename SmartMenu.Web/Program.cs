980033df8169ab679cd20eb48ef4c07d931127b0
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-18 17:56:23 +0200

    add coupon service

--- SmartMenu.Web/Program.cs
+++ SmartMenu.Web/Program.cs
@@ -1,7 +1,18 @@
+using SmartMenu.Web.Service.IService;
+using SmartMenu.Web.Service;
+using SmartMenu.Web.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
--- SmartMenu.Web/Service/IService/ICouponService.cs
+++ SmartMenu.Web/Service/IService/ICouponService.cs
@@ -1,6 +1,14 @@
-﻿namespace SmartMenu.Web.Service.IService
+﻿using SmartMenu.Web.Models;
+
+namespace SmartMenu.Web.Service.IService
 {
     public interface ICouponService
     {
+        Task<ResponseDto?> GetAsync(string couponCode);
+        Task<ResponseDto?> GetAllAsync();
+        Task<ResponseDto?> GetByIdAsync(int id);
+        Task<ResponseDto?> CreateAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteAsync(int id);
     }
 }
