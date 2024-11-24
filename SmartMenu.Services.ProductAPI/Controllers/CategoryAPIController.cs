5d042fa9778b18c1b8b50019d802da8ff9e9039f
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-24 17:00:59 +0200

    added crud methods

--- SmartMenu.Services.ProductAPI/Controllers/CategoryAPIController.cs
+++ SmartMenu.Services.ProductAPI/Controllers/CategoryAPIController.cs
@@ -22,6 +22,101 @@ namespace SmartMenu.Services.ProductAPI.Controllers
             _response = new ResponseDto();
         }
 
-       
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Category> objList = _db.Categories.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CategoryDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Category obj = _db.Categories.First(u => u.CategoryId == id);
+                _response.Result = _mapper.Map<CategoryDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Post(CategoryDto CategoryDto)
+        {
+            try
+            {
+                Category category = _mapper.Map<Category>(CategoryDto);
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+
+
+                _response.Result = _mapper.Map<CategoryDto>(category);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put(CategoryDto CategoryDto)
+        {
+            try
+            {
+                Category category = _mapper.Map<Category>(CategoryDto);
+
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CategoryDto>(category);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Category obj = _db.Categories.First(u => u.CategoryId == id);
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
     }
 }
