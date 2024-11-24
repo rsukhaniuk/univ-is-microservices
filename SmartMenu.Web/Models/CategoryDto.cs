7117969ff071ddf6fc22b04703dd443a4d33407b
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-24 17:52:02 +0200

    added category dto

---
+++ SmartMenu.Web/Models/CategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using SmartMenu.Web.Utility;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenu.Web.Models
+{
+    public class CategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
