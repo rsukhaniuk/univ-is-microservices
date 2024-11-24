4452e4d5d7347c656bad5333c5d3d09df14798d5
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-24 15:25:03 +0200

    added product category model

---
+++ SmartMenu.Services.ProductAPI/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartMenu.Services.ProductAPI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
