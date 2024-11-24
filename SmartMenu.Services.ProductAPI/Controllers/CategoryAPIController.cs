2d9c36239f2f392885d7eb9d8583ad78590f7f00
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-24 16:59:04 +0200

    started implementing category controller

---
+++ SmartMenu.Services.ProductAPI/Controllers/CategoryAPIController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using SmartMenu.Services.ProductAPI.Data;
+using SmartMenu.Services.ProductAPI.Models;
+using SmartMenu.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartMenu.Services.ProductAPI.Controllers
+{
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+
+        public CategoryAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+
+       
+    }
+}
